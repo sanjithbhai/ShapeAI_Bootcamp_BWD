1a46a5a8684d03b126d9dae53e86d1e7feb1d96c
sanjithbhai <85478816+sanjithbhai@users.noreply.github.com>
2021-06-07 12:26:54 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+function Note(){
+  return (
+ <div className="note">
+   <h1>Javascript and React.js</h1>
+   <p>The bootcamp was an amazing one. BUt Iam not able to  understand the css file part</p>
+   </div>
+    );
+}
+
+export default Note;
